0e0bf82f12debf9edde3a648cd4bcbfc74aff37f
Claudio Merino <claudio@elbuencamino.com>
2016-08-09 04:19:58 -0400

    primera subida catálogo de plantillas

---
+++ wom-base/js/main.js
@@ -0,0 +1,16 @@
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Closes the Responsive Menu on Click outside Menu
+$('body > *').not('nav').click(function() {
+    if(!$('button.navbar-toggle').hasClass('collapsed')) {
+        $('.navbar-toggle:visible').click();
+    }
+});
+// Open overlay
+$('#toggle').click(function() {
+	$(this).toggleClass('active');
+	$('#overlay').toggleClass('open');
+ });
